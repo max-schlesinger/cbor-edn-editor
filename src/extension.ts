021f213d46071150e2e52de57a7580234e497155
max-schlesinger <maxschlesinger2acc@gmail.com>
2026-03-16 19:10:23 +0100

    quick fix

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,15 @@ import * as vscode from "vscode";
 import { CborEditorProvider } from "./cborEditor";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log("cbor-edn-editor: activate called");
   context.subscriptions.push(CborEditorProvider.register(context));
+
   context.subscriptions.push(
     vscode.commands.registerCommand("cbor-edn-editor.Cbor.new", async () => {
-      const newUri = vscode.Uri.parse("untitled:new.cbor");
+      // helps to create multiple new files without name conflicts
+      const timestamp = new Date().getTime();
+      const newUri = vscode.Uri.parse(`untitled:new-${timestamp}.cbor`);
 
       try {
-        await vscode.workspace.openTextDocument(newUri);
-
         await vscode.commands.executeCommand(
           "vscode.openWith",
           newUri,
